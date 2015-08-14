3118b348268220cb28438ff3b2c016ce1766bd58
eshelyho <eshely.ho@gmail.com>
2015-08-14 14:59:55 +0800

    completed exercise 1

--- CSExercises/Ex01-06/Ex01.cs
+++ CSExercises/Ex01-06/Ex01.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CSExercises
 {
     public class Ex01
     {
         public static void Main(string[] args)
-        {
+        {System.Console.WriteLine("Please enter your name");
+        string name = System.Console.ReadLine();
+        System.Console.WriteLine("Good Morning {0}", name);
             
         }
     }
