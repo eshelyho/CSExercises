21bfb7b48e93216f2a8ebdc9a206b93d45379eef
eshelyho <eshely.ho@gmail.com>
2015-08-16 15:56:01 +0800

    ex02 done by HE JIAYING

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+
+
 
 namespace CSExercises
 {
@@ -10,7 +13,17 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("Please enter the salar");
+            String s = System.Console.ReadLine();
+
+            double i = System.Convert.ToInt32(s);
+
+            double sum = i * 1.13;
+
+            NumberFormatInfo curr = new CultureInfo("en-US", false).NumberFormat;
+
+            System.Console.WriteLine("Total income salary is " + sum.ToString("C", curr));
+
         }
     }
 }
--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("Please enter the side a.");
+            string a1 = System.Console.ReadLine();
+            double a = System.Convert.ToDouble(a1);
+
+            System.Console.WriteLine("Please enter the side b.");
+            string b1 = System.Console.ReadLine();
+            double b = System.Convert.ToDouble(b1);
+
+            System.Console.WriteLine("Please enter the side c.");
+            string c1 = System.Console.ReadLine();
+            double c = System.Convert.ToDouble(c1);
+
+            if ((a < 1) || ( b<1 ) || (c < 1))
+            {
+                System.Console.WriteLine("Please enter the right number!");
+            }
+            else
+            {
+                if ((a + b < c) || (a + c < b) || (c + b < a))
+                    {
+                        double s = (a + b + c) / 2;
+                        double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+                    }
+
+                
+                else
+                {
+                    System.Console.WriteLine("Please enter the right number!");
+                }
+
+            }
+
+
         }
     }
 }
